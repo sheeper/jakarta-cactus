26411ea9beada31ea420c42150fba539743f876b
vmassol <vmassol@13f79535-47bb-0310-9956-ffa450edef68>
2002-10-19 16:44:09 +0000

    Removed unused members and started to correct checkstyle errors
    git-svn-id: http://svn.apache.org/repos/asf/jakarta/cactus/trunk@236755 13f79535-47bb-0310-9956-ffa450edef68

--- Eclipse-Plugin/src/java/org/apache/cactus/eclipse/ui/CactusPlugin.java
+++ Eclipse-Plugin/src/java/org/apache/cactus/eclipse/ui/CactusPlugin.java
@@ -56,85 +56,51 @@
  */
 package org.apache.cactus.eclipse.ui;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.AbstractSet;
-import java.util.HashSet;
-
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPluginDescriptor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.debug.core.DebugPlugin;
-import org.eclipse.debug.core.ILaunch;
-import org.eclipse.debug.core.ILaunchConfiguration;
-import org.eclipse.debug.core.ILaunchListener;
-import org.eclipse.debug.core.ILaunchManager;
-import org.eclipse.jdt.core.IJavaElement;
-import org.eclipse.jdt.core.IType;
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.internal.junit.launcher.JUnitBaseLaunchConfiguration;
-import org.eclipse.jdt.internal.junit.ui.TestRunnerViewPart;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 /**
- * @author Julien Ruaux
- *
  * The plug-in runtime class for the Cactus plug-in.
  * 
- * @version $Id: $
+ * @version $Id:$
+ * @author <a href="mailto:jruaux@octo.com">Julien Ruaux</a>
+ * @author <a href="mailto:vmassol@apache.org">Vincent Massol</a>
  */
-public class CactusPlugin extends AbstractUIPlugin implements ILaunchListener
+public class CactusPlugin extends AbstractUIPlugin
 {
     /**
      * The single instance of this plug-in runtime class.
      */
-    private static CactusPlugin fgPlugin = null;
-
-    public static final String PLUGIN_ID = "org.apache.cactus.eclipse";
-
-    public static final String TEST_SUPERCLASS_NAME = 
-        "junit.framework.TestCase";
-    public static final String TEST_INTERFACE_NAME = "junit.framework.Test";
-
-    private static URL fgIconBaseURL;
+    private static CactusPlugin plugin;
 
     /**
-     * Use to track new launches. We need to do this
-     * so that we only attach a TestRunner once to a launch.
-     * Once a test runner is connected it is removed from the set.
+     * @see org.eclipse.core.runtime.Plugin#Plugin(IPluginDescriptor)
      */
-    private AbstractSet fTrackedLaunches = new HashSet(20);
-
     public CactusPlugin(IPluginDescriptor theDescription)
     {
         super(theDescription);
-        fgPlugin = this;
-        String pathSuffix = "icons/";
-        try
-        {
-            fgIconBaseURL =
-                new URL(getDescriptor().getInstallURL(), pathSuffix);
-        } 
-        catch (MalformedURLException e)
-        {
-            // do nothing
-        }
+        plugin = this;
     }
 
+    /**
+     * @return the single instance of this plug-in runtime class
+     */
     public static CactusPlugin getDefault()
     {
-        return fgPlugin;
+        return plugin;
     }
 
+    /**
+     * @return the active workbench shell
+     */
     public static Shell getActiveWorkbenchShell()
     {
         IWorkbenchWindow workBenchWindow = getActiveWorkbenchWindow();
@@ -146,17 +112,15 @@ public class CactusPlugin extends AbstractUIPlugin implements ILaunchListener
     }
 
     /**
-     * Returns the active workbench window
-     * 
      * @return the active workbench window
      */
     public static IWorkbenchWindow getActiveWorkbenchWindow()
     {
-        if (fgPlugin == null)
+        if (plugin == null)
         {
             return null;
         }
-        IWorkbench workBench = fgPlugin.getWorkbench();
+        IWorkbench workBench = plugin.getWorkbench();
         if (workBench == null)
         {
             return null;
@@ -164,22 +128,9 @@ public class CactusPlugin extends AbstractUIPlugin implements ILaunchListener
         return workBench.getActiveWorkbenchWindow();
     }
 
-    public IWorkbenchPage getActivePage()
-    {
-        return getActiveWorkbenchWindow().getActivePage();
-    }
-
     public static String getPluginId()
     {
         return getDefault().getDescriptor().getUniqueIdentifier();
-    }
-
-    /*
-     * @see AbstractUIPlugin#initializeDefaultPreferences
-     */
-    protected void initializeDefaultPreferences(IPreferenceStore theStore)
-    {
-        super.initializeDefaultPreferences(theStore);
     }
 
     public static void log(Throwable theThrowable)
@@ -191,127 +142,6 @@ public class CactusPlugin extends AbstractUIPlugin implements ILaunchListener
     public static void log(IStatus theStatus)
     {
         getDefault().getLog().log(theStatus);
-    }
-
-    /*
-     * @see ILaunchListener#launchRemoved(ILaunch)
-     */
-    public void launchRemoved(ILaunch theLaunch)
-    {
-        fTrackedLaunches.remove(theLaunch);
-    }
-
-    /*
-     * @see ILaunchListener#launchAdded(ILaunch)
-     */
-    public void launchAdded(ILaunch theLaunch)
-    {
-        fTrackedLaunches.add(theLaunch);
-    }
-
-    public void connectTestRunner(ILaunch launch, IType launchedType, int port)
-    {
-        IWorkbench workbench = getWorkbench();
-        if (workbench == null)
-        {
-            return;
-        }
-        
-        IWorkbenchWindow window = getWorkbench().getActiveWorkbenchWindow();
-        IWorkbenchPage page = window.getActivePage();
-        TestRunnerViewPart testRunner = null;
-        if (page != null)
-        {
-            try
-            { // show the result view if it isn't shown yet
-                testRunner =
-                    (TestRunnerViewPart) page.findView(TestRunnerViewPart.NAME);
-                if (testRunner == null)
-                {
-                    IWorkbenchPart activePart = page.getActivePart();
-                    testRunner =
-                        (TestRunnerViewPart) page.showView(
-                            TestRunnerViewPart.NAME);
-                    //restore focus stolen by the creation of the result view
-                    page.activate(activePart);
-                }
-            } 
-            catch (PartInitException pie)
-            {
-                log(pie);
-            }
-        }
-        if (testRunner != null)
-        {
-            testRunner.startTestRunListening(launchedType, port, launch);
-        }
-    }
-
-    /*
-     * @see ILaunchListener#launchChanged(ILaunch)
-     */
-    public void launchChanged(final ILaunch launch)
-    {
-        if (!fTrackedLaunches.contains(launch))
-        {
-            return;
-        }
-
-        ILaunchConfiguration config = launch.getLaunchConfiguration();
-        IType launchedType = null;
-        int port = -1;
-        if (config != null)
-        {
-            // test whether the launch defines the JUnit attributes
-            String portStr =
-                launch.getAttribute(JUnitBaseLaunchConfiguration.PORT_ATTR);
-            String typeStr =
-                launch.getAttribute(JUnitBaseLaunchConfiguration.TESTTYPE_ATTR);
-            if (portStr != null && typeStr != null)
-            {
-                port = Integer.parseInt(portStr);
-                IJavaElement element = JavaCore.create(typeStr);
-                if (element instanceof IType)
-                {
-                    launchedType = (IType) element;
-                }
-            }
-        }
-        if (launchedType != null)
-        {
-            fTrackedLaunches.remove(launch);
-            final int finalPort = port;
-            final IType finalType = launchedType;
-            getDisplay().asyncExec(new Runnable()
-            {
-                public void run()
-                {
-                    connectTestRunner(launch, finalType, finalPort);
-                }
-            });
-        }
-    }
-
-    /*
-     * @see Plugin#startup()
-     */
-    public void startup() throws CoreException
-    {
-        super.startup();
-        ILaunchManager launchManager =
-            DebugPlugin.getDefault().getLaunchManager();
-        launchManager.addLaunchListener(this);
-    }
-
-    /*
-     * @see Plugin#shutdown()
-     */
-    public void shutdown() throws CoreException
-    {
-        super.shutdown();
-        ILaunchManager launchManager =
-            DebugPlugin.getDefault().getLaunchManager();
-        launchManager.removeLaunchListener(this);
     }
 
     public static Display getDisplay()
