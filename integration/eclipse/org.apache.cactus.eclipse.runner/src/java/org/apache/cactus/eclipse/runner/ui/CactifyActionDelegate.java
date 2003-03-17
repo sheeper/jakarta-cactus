ebd45ae6d2cfec595993987958b0a0a9df39951c
jruaux <jruaux@13f79535-47bb-0310-9956-ffa450edef68>
2003-03-17 09:45:15 +0000

    Now cactifies a project by adding framework jars to the project's classpath
    git-svn-id: http://svn.apache.org/repos/asf/jakarta/cactus/trunk@237433 13f79535-47bb-0310-9956-ffa450edef68

--- integration/eclipse/org.apache.cactus.eclipse.runner/src/java/org/apache/cactus/eclipse/runner/ui/CactifyActionDelegate.java
+++ integration/eclipse/org.apache.cactus.eclipse.runner/src/java/org/apache/cactus/eclipse/runner/ui/CactifyActionDelegate.java
@@ -56,8 +56,19 @@
  */
 package org.apache.cactus.eclipse.runner.ui;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.net.URL;
+import java.util.Vector;
+
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 
@@ -69,12 +80,32 @@ import org.eclipse.ui.IWorkbenchPart;
  */
 public class CactifyActionDelegate implements IObjectActionDelegate
 {
-
+    /**
+     * The project selected by the user
+     */
+    private IJavaProject selectedProject;
+    /**
+     * The active part
+     */
+    private IWorkbenchPart part;
+    /**
+     * Path to the plugin's library directory
+     */
+    private static final String CACTUS_LIBRARY_PATH = "./ant/lib/";
+    /**
+     * Name of the common libraries folder
+     */
+    private static final String CACTUS_COMMON_LIBRARY_PATH = "common";
+    /**
+     * Name of the client libraries folder
+     */
+    private static final String CACTUS_CLIENT_LIBRARY_PATH = "client";
     /**
      * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
      */
     public void setActivePart(IAction theAction, IWorkbenchPart theTargetPart)
     {
+        this.part = theTargetPart;
     }
 
     /**
@@ -82,10 +113,53 @@ public class CactifyActionDelegate implements IObjectActionDelegate
      */
     public void run(IAction theAction)
     {
-        CactusPlugin.displayErrorMessage(
-            "Cactify",
-            "this will be implemented soon !",
-            null);
+        if (part != null && selectedProject != null)
+        {
+            CactusPlugin thePlugin = CactusPlugin.getDefault();
+            URL commonLibURL =
+                thePlugin.find(
+                    new Path(CACTUS_LIBRARY_PATH + CACTUS_COMMON_LIBRARY_PATH));
+            URL clientLibURL =
+                thePlugin.find(
+                    new Path(CACTUS_LIBRARY_PATH + CACTUS_CLIENT_LIBRARY_PATH));
+            File commonLibDir = new File(commonLibURL.getPath());
+            File clientLibDir = new File(clientLibURL.getPath());
+            IClasspathEntry[] commonEntries =
+                getLibClassPathEntries(commonLibDir);
+            IClasspathEntry[] clientEntries =
+                getLibClassPathEntries(clientLibDir);
+            IClasspathEntry[] allNewEntries =
+                new IClasspathEntry[commonEntries.length
+                    + clientEntries.length];
+            System.arraycopy(
+                commonEntries,
+                0,
+                allNewEntries,
+                0,
+                commonEntries.length);
+            System.arraycopy(
+                clientEntries,
+                0,
+                allNewEntries,
+                commonEntries.length,
+                clientEntries.length);
+            try
+            {
+
+                IClasspathEntry[] existingEntries =
+                    selectedProject.getRawClasspath();
+                selectedProject.setRawClasspath(
+                    merge(existingEntries, allNewEntries),
+                    null);
+            }
+            catch (JavaModelException e)
+            {
+                CactusPlugin.displayErrorMessage(
+                    CactusMessages.getString("Cactify.message.error"),
+                    e.getMessage(),
+                    null);
+            }
+        }
     }
 
     /**
@@ -93,6 +167,96 @@ public class CactifyActionDelegate implements IObjectActionDelegate
      */
     public void selectionChanged(IAction theAction, ISelection theSelection)
     {
+        selectedProject = null;
+        if (theSelection instanceof IStructuredSelection)
+        {
+            IStructuredSelection structuredSelection =
+                (IStructuredSelection) theSelection;
+            if (structuredSelection.size() == 1)
+            {
+                Object selectedResource = structuredSelection.getFirstElement();
+                if (selectedResource instanceof IJavaProject)
+                {
+                    selectedProject = (IJavaProject) selectedResource;
+                }
+            }
+        }
     }
 
+    /**
+     * Returns a list of entries of jars files contained
+     * in the given directory. 
+     * @param theDirectory the directory to list jars from
+     * @return an array of jar entries
+     */
+    private IClasspathEntry[] getLibClassPathEntries(File theDirectory)
+    {
+        File[] jarFiles = theDirectory.listFiles(new JarFilenameFilter());
+        IClasspathEntry[] result = new IClasspathEntry[jarFiles.length];
+        for (int i = 0; i < jarFiles.length; i++)
+        {
+            File currentJarFile = jarFiles[i];
+            result[i] =
+                JavaCore.newLibraryEntry(
+                    new Path(currentJarFile.getAbsolutePath()),
+                    null,
+                    null);
+        }
+        return result;
+    }
+    /**
+     * Filter for jar files.
+     * i.e. accepts files like 'library.jar'
+     * 
+     * @author <a href="mailto:jruaux@octo.com">Julien Ruaux</a>
+     * 
+     * @version $Id$
+     */
+    static class JarFilenameFilter implements FilenameFilter
+    {
+        /**
+         * @see java.io.FilenameFilter#accept(java.io.File, java.lang.String)
+         */
+        public boolean accept(File theDir, String theFilename)
+        {
+            return theFilename.endsWith(".jar");
+        }
+    }
+
+    /**
+     * @param theFirstArray an array of classpath entries
+     * @param theSecondArray an array of classpath entries
+     * @return the fusion of the two given arrays 
+     */
+    private static IClasspathEntry[] merge(
+        IClasspathEntry[] theFirstArray,
+        IClasspathEntry[] theSecondArray)
+    {
+
+        Vector result = new Vector();
+        for (int i = 0; i < theFirstArray.length; i++)
+        {
+            result.add(theFirstArray[i]);
+        }
+        for (int i = 0; i < theSecondArray.length; i++)
+        {
+            IClasspathEntry currentEntry = theSecondArray[i];
+            boolean entryAlreadyExists = false;
+            for (int j = 0; j < theFirstArray.length; j++)
+            {
+                IClasspathEntry comparedEntry = theFirstArray[j];
+                if (comparedEntry.getPath().equals(currentEntry.getPath()))
+                {
+                    entryAlreadyExists = true;
+                    break;
+                }
+            }
+            if (!entryAlreadyExists)
+            {
+                result.add(currentEntry);
+            }
+        }
+        return (IClasspathEntry[]) result.toArray(
+            new IClasspathEntry[result.size()]);
+    }
 }
