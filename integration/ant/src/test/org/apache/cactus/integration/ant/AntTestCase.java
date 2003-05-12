80eaf45f75c8840e3fa54690ec291ceb2ec93418
cmlenz <cmlenz@13f79535-47bb-0310-9956-ffa450edef68>
2003-05-12 09:05:24 +0000

    Starting the merge from CACTUS_14_ANT_BRANCH  - Adding Ant task unit tests
    git-svn-id: http://svn.apache.org/repos/asf/jakarta/cactus/trunk@237856 13f79535-47bb-0310-9956-ffa450edef68

---
+++ integration/ant/src/test/org/apache/cactus/integration/ant/AntTestCase.java
@@ -0,0 +1,314 @@
+/*
+ * ====================================================================
+ *
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2003 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Cactus" and "Apache Software
+ *    Foundation" must not be used to endorse or promote products
+ *    derived from this software without prior written permission. For
+ *    written permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+package org.apache.cactus.integration.ant;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.AssertionFailedError;
+import junit.framework.TestCase;
+
+import org.apache.tools.ant.BuildEvent;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.BuildListener;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.ProjectHelper;
+import org.apache.tools.ant.Target;
+
+/**
+ * An AntTestCase is a TestCase specialization for unit testing Ant tasks.
+ * 
+ * @author <a href="mailto:cmlenz@gmx.de">Christopher Lenz</a>
+ *
+ * @version $Id$
+ */
+public abstract class AntTestCase extends TestCase implements BuildListener
+{
+
+    // Instance Variables ------------------------------------------------------
+
+    /**
+     * The Ant project.
+     */
+    private Project project;
+
+    /**
+     * The name of the test build file.
+     */
+    private String buildFile;
+
+    /**
+     * Buffer containing all messages logged by Ant. 
+     */
+    private Map log = new HashMap();
+
+    // Constructors ------------------------------------------------------------
+
+    /**
+     * Constructor
+     * 
+     * @param theName The name of the test
+     * @param theBuildFile The Ant build file corresponding to the test fixture
+     */
+    public AntTestCase(String theName, String theBuildFile)
+    {
+        super(theName);
+        this.buildFile = theBuildFile;
+    }
+
+    // TestCase Implementation -------------------------------------------------
+
+    /**
+     * Initializes a fresh Ant project with a target named after the name of the
+     * test case.
+     * 
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception
+    {
+        this.project = new Project();
+        this.project.addBuildListener(this);
+        this.project.init();
+        ProjectHelper.configureProject(this.project, new File(this.buildFile));
+        if (getProject().getTargets().get("setUp") != null)
+        {
+            getProject().executeTarget("setUp");
+        }
+    }
+
+    /**
+     * @see junit.framework.TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception
+    {
+        if (getProject().getTargets().get("tearDown") != null)
+        {
+            try
+            {
+                getProject().executeTarget("tearDown");
+            }
+            catch (BuildException be)
+            {
+                System.err.println("Exception in tearDown: " + be.getMessage());
+            }
+        }
+    }
+
+    // Protected Methods -------------------------------------------------------
+
+    /**
+     * Asserts that a specific message has been logged at a specific log level.
+     * 
+     * @param theMessage The message to check for
+     * @param theLogLevel The log level of the message
+     * @throws IOException If an error occurred reading the log buffer
+     */
+    protected void assertMessageLogged(String theMessage, int theLogLevel)
+        throws IOException
+    {
+        StringBuffer buffer = (StringBuffer) log.get(new Integer(theLogLevel));
+        if (buffer != null)
+        {
+            BufferedReader reader =
+                new BufferedReader(new StringReader(buffer.toString()));
+            String line = null;
+            while ((line = reader.readLine()) != null)
+            {
+                if (line.equals(theMessage))
+                {
+                    return;
+                }
+            }
+        }
+        throw new AssertionFailedError(
+            "Expected log message '" + theMessage + "'");
+    }
+
+    /**
+     * Asserts that a message containing the specified substring has been logged
+     * at a specific log level.
+     * 
+     * @param theSubstring The substring to check for
+     * @param theLogLevel The log level of the message
+     * @throws IOException If an error occurred reading the log buffer
+     */
+    protected void assertMessageLoggedContaining(String theSubstring,
+        int theLogLevel)
+        throws IOException
+    {
+        StringBuffer buffer = (StringBuffer) log.get(new Integer(theLogLevel));
+        if (buffer != null)
+        {
+            BufferedReader reader =
+                new BufferedReader(new StringReader(buffer.toString()));
+            String line = null;
+            while ((line = reader.readLine()) != null)
+            {
+                if (line.indexOf(theSubstring) >= 0)
+                {
+                    return;
+                }
+            }
+        }
+        throw new AssertionFailedError(
+            "Expected log message containing '" + theSubstring + "'");
+    }
+
+    /**
+     * Executes the target in the project that corresponds to the current test
+     * case.
+     */
+    protected void executeTestTarget()
+    {
+        this.project.executeTarget(getName());
+    }
+
+    /**
+     * Returns the Ant project.
+     * 
+     * @return The project
+     */
+    protected Project getProject()
+    {
+        return this.project;
+    }
+
+    /**
+     * Returns the base directory of the Ant project.
+     * 
+     * @return The base directory
+     */
+    protected File getProjectDir()
+    {
+        return this.project.getBaseDir();
+    }
+
+    /**
+     * Returns the target in the project that corresponds to the current test
+     * case.
+     * 
+     * @return The test target
+     */
+    protected Target getTestTarget()
+    {
+        return (Target) getProject().getTargets().get(getName());
+    }
+
+    // BuildListener Implementation ----------------------------------------
+
+    /**
+     * @see BuildListener#buildStarted
+     */
+    public void buildStarted(BuildEvent theEvent)
+    {
+    }
+
+    /**
+     * @see BuildListener#buildFinished
+     */
+    public void buildFinished(BuildEvent theEvent)
+    {
+    }
+
+    /**
+     * @see BuildListener#targetStarted
+     */
+    public void targetStarted(BuildEvent theEvent)
+    {
+    }
+
+    /**
+     * @see BuildListener#targetFinished
+     */
+    public void targetFinished(BuildEvent theEvent)
+    {
+    }
+
+    /**
+     * @see BuildListener#taskStarted
+     */
+    public void taskStarted(BuildEvent theEvent)
+    {
+    }
+
+    /**
+     * @see BuildListener#taskFinished
+     */
+    public void taskFinished(BuildEvent theEvent)
+    {
+    }
+
+    /**
+     * @see BuildListener#messageLogged
+     */
+    public void messageLogged(BuildEvent theEvent)
+    {
+        StringBuffer buffer = (StringBuffer)
+            log.get(new Integer(theEvent.getPriority()));
+        if (buffer == null)
+        {
+            buffer = new StringBuffer();
+            log.put(new Integer(theEvent.getPriority()), buffer);
+        }
+        buffer.append(theEvent.getMessage());
+    }
+
+}
---
+++ integration/ant/src/test/org/apache/cactus/integration/ant/TestCactifyTask.java
@@ -0,0 +1,344 @@
+/*
+ * ====================================================================
+ *
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2003 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Cactus" and "Apache Software
+ *    Foundation" must not be used to endorse or promote products
+ *    derived from this software without prior written permission. For
+ *    written permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+package org.apache.cactus.integration.ant;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.jar.JarFile;
+
+import org.apache.cactus.integration.ant.webxml.WebXml;
+import org.apache.cactus.integration.ant.webxml.WebXmlIo;
+import org.apache.tools.ant.BuildException;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ * Unit tests for {@link CactifyTask}.
+ * 
+ * TODO: test whether all files contained by the source WAR are also added to
+ *       the cactified WAR
+ * TODO: test whether the nested redirector definitions get inserted into the
+ *       cactified web.xml correctly
+ * TODO: test whether the mergewebxml is actually merged into the cactified 
+ *       web.xml  
+ * 
+ * @author <a href="mailto:cmlenz@apache.org">Christopher Lenz</a>
+ *
+ * @version $Id$
+ */
+public final class TestCactifyTask extends AntTestCase
+{
+
+    // Inner Classes -----------------------------------------------------------
+
+    /**
+     * Entity resolver implementation that simply returns <code>null</code>
+     * for each request. 
+     */
+    private static class NullEntityResolver implements EntityResolver
+    {
+
+        /**
+         * @see org.xml.sax.EntityResolver#resolveEntity
+         */
+        public InputSource resolveEntity(String thePublicId, String theSystemId)
+            throws SAXException, IOException
+        {
+            return null;
+        }
+        
+    }
+
+    // Constants ---------------------------------------------------------------
+
+    /**
+     * The build file corresponding to this fixture.
+     */
+    private static final String TEST_BUILD_FILE =
+        "src/test-input/org/apache/cactus/integration/ant/test-cactify.xml";
+
+    // Constructors ------------------------------------------------------------
+
+    /**
+     * @see TestCase#TestCase(String)
+     */
+    public TestCactifyTask(String theTestName)
+    {
+        super(theTestName, TEST_BUILD_FILE);
+    }
+
+    // TestCase Implementation -------------------------------------------------
+
+    /**
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+
+        getProject().addTaskDefinition("cactify", CactifyTask.class);
+    }
+
+    // Test Methods ------------------------------------------------------------
+
+    /**
+     * Verifies that the task throws an exception when the srcfile attribute 
+     * has not been set.
+     * 
+     * @throws Exception If an unexpected error occurs
+     */
+    public void testSrcFileNotSet() throws Exception
+    {
+        try
+        {
+            executeTestTarget();
+            fail("Expected BuildException");
+        }
+        catch (BuildException expected)
+        {
+            assertEquals("The [srcfile] attribute is required",
+                expected.getMessage());
+        }
+    }
+
+    /**
+     * Verifies that the task throws an exception when the destfile attribute 
+     * has not been set.
+     * 
+     * @throws Exception If an unexpected error occurs
+     */
+    public void testDestFileNotSet() throws Exception
+    {
+        try
+        {
+            executeTestTarget();
+            fail("Expected BuildException");
+        }
+        catch (BuildException expected)
+        {
+            assertEquals("You must specify the war file to create!",
+                expected.getMessage());
+        }
+    }
+
+    /**
+     * Tests whether the Cactus test redirectors are correctly added to the 
+     * descriptor of the cactified WAR. 
+     * 
+     * @throws Exception If an unexpected error occurs
+     */
+    public void testDefaultRedirectorsNoDoctype() throws Exception
+    {
+        executeTestTarget();
+
+        File destFile = getProject().resolveFile("work/destfile.war");
+        JarFile destWar = new JarFile(destFile);
+        try
+        {
+            WebXml webXml = WebXmlIo.parseWebXml(destWar,
+                new NullEntityResolver());
+    
+            assertServletMapping(webXml,
+                "org.apache.cactus.server.ServletTestRedirector",
+                "/ServletRedirector");
+            assertJspMapping(webXml, "/jspRedirector.jsp", "/JspRedirector");
+    
+            // As the deployment descriptor in the source WAR doesn't contain a 
+            // DOCTYPE, it is assumed to be a version 2.2 descriptor. Thus it 
+            // should not contain a definition of the filter test redirector.
+            // Assert that.
+            assertTrue("Filter test redirector should not have been defined",
+                !webXml.getFilterNames().hasNext());
+        }
+        finally
+        {
+            destWar.close();
+        }
+   }
+
+    /**
+     * Tests whether the Cactus test redirectors are correctly added to the 
+     * descriptor of the cactified WAR. 
+     * 
+     * @throws Exception If an unexpected error occurs
+     */
+    public void testDefaultRedirectors22Doctype() throws Exception
+    {
+        executeTestTarget();
+
+        File destFile = getProject().resolveFile("work/destfile.war");
+        JarFile destWar = new JarFile(destFile);
+        try
+        {
+            WebXml webXml = WebXmlIo.parseWebXml(destWar,
+                new NullEntityResolver());
+
+            assertServletMapping(webXml,
+                "org.apache.cactus.server.ServletTestRedirector",
+                "/ServletRedirector");
+            assertJspMapping(webXml, "/jspRedirector.jsp", "/JspRedirector");
+            assertTrue("Filter test redirector should not have been defined",
+                !webXml.getFilterNames().hasNext());
+        }
+        finally
+        {
+            destWar.close();
+        }
+    }
+
+    /**
+     * Tests whether the Cactus test redirectors are correctly added to the 
+     * descriptor of the cactified WAR. 
+     * 
+     * @throws Exception If an unexpected error occurs
+     */
+    public void testDefaultRedirectors23Doctype() throws Exception
+    {
+        executeTestTarget();
+
+        File destFile = getProject().resolveFile("work/destfile.war");
+        JarFile destWar = new JarFile(destFile);
+        try
+        {
+            WebXml webXml = WebXmlIo.parseWebXml(destWar,
+                new NullEntityResolver());
+            
+            assertServletMapping(webXml,
+                "org.apache.cactus.server.ServletTestRedirector",
+                "/ServletRedirector");
+            assertJspMapping(webXml, "/jspRedirector.jsp", "/JspRedirector");
+            assertFilterMapping(webXml,
+                "org.apache.cactus.server.FilterTestRedirector",
+                "/FilterRedirector");
+        }
+        finally
+        {
+            destWar.close();
+        }
+    }
+
+    // Private Methods ---------------------------------------------------------
+
+    /**
+     * Asserts that a filter of the specified class is defined in the given
+     * deployment descriptor and mapped to a specific URL-pattern.
+     * 
+     * @param theWebXml The deployment descriptor
+     * @param theFilterClass The name of the filter class
+     * @param theMapping The URL-pattern that the filter is expected to be
+     *        mapped to
+     */
+    private void assertFilterMapping(WebXml theWebXml, String theFilterClass,
+        String theMapping)
+    {
+        Iterator names = theWebXml.getFilterNamesForClass(theFilterClass);
+        assertTrue("Definition of " + theFilterClass + " not found",
+            names.hasNext());
+        String name = (String) names.next();
+        Iterator mappings = theWebXml.getFilterMappings(name);
+        assertTrue("Mapping for " + theFilterClass + " not found",
+            mappings.hasNext());
+        assertEquals(theMapping, mappings.next());
+    }
+
+    /**
+     * Asserts that the specified JSP file is defined in the given deployment
+     * descriptor and mapped to a specific URL-pattern.
+     * 
+     * @param theWebXml The deployment descriptor
+     * @param theJspFile The JSP file name
+     * @param theMapping The URL-pattern that the JSP file is expected to be
+     *        mapped to
+     */
+    private void assertJspMapping(WebXml theWebXml, String theJspFile,
+        String theMapping)
+    {
+        Iterator names = theWebXml.getServletNamesForJspFile(theJspFile);
+        assertTrue("Definition of " + theJspFile + " not found",
+            names.hasNext());
+        String name = (String) names.next();
+        Iterator mappings = theWebXml.getServletMappings(name);
+        assertTrue("Mapping for " + theJspFile + " not found",
+            mappings.hasNext());
+        assertEquals(theMapping, mappings.next());
+    }
+
+    /**
+     * Asserts that a servlet of the specified class is defined in the given
+     * deployment descriptor and mapped to a specific URL-pattern.
+     * 
+     * @param theWebXml The deployment descriptor
+     * @param theServletClass The name of servlet class
+     * @param theMapping The URL-pattern that the servlet is expected to be
+     *        mapped to
+     */
+    private void assertServletMapping(WebXml theWebXml, String theServletClass,
+        String theMapping)
+    {
+        Iterator names = theWebXml.getServletNamesForClass(theServletClass);
+        assertTrue("Definition of " + theServletClass + " not found",
+            names.hasNext());
+        String name = (String) names.next();
+        Iterator mappings = theWebXml.getServletMappings(name);
+        assertTrue("Mapping for " + theServletClass + " not found",
+            mappings.hasNext());
+        assertEquals(theMapping, mappings.next());
+    }
+
+}
