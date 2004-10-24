e37ff6a6e3e1ddb55826c56a4e52dc45395a7b09
felipeal <felipeal@13f79535-47bb-0310-9956-ffa450edef68>
2004-10-24 05:13:28 +0000

    test case for cactus.sysproperties
    git-svn-id: http://svn.apache.org/repos/asf/jakarta/cactus/trunk@239056 13f79535-47bb-0310-9956-ffa450edef68

---
+++ integration/maven/src/plugin-test/testCactusSysProperties/src/test-cactus/org/apache/cactus/test/TestProperties.java
@@ -0,0 +1,43 @@
+/* 
+ * ========================================================================
+ * 
+ * Copyright 2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * ========================================================================
+ */
+package org.apache.cactus.test05;
+
+import java.util.Map;
+
+import org.apache.cactus.ServletTestCase;
+import org.apache.cactus.WebRequest;
+
+public class TestProperties extends ServletTestCase
+{
+
+    /**
+     * Verify that the properties were set correctly.
+     */
+    public void testProperties()
+    {
+      String prop1 = System.getProperty("prop1");
+      String prop2 = System.getProperty("prop2");
+      assertNotNull( prop1 );
+      assertNotNull( prop2 );
+      assertEquals( "value1", prop1 );
+      assertEquals( "value2", prop2 );
+    }
+
+}
