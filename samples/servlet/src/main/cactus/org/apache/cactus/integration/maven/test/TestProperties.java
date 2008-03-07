ec5c08250d2ab737e3f2a4b82dc96c2809f61ffd
ptahchiev <ptahchiev@13f79535-47bb-0310-9956-ffa450edef68>
2008-03-07 14:53:57 +0000

    Some testcases added.
    git-svn-id: http://svn.apache.org/repos/asf/jakarta/cactus/trunk@634700 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/servlet/src/main/cactus/org/apache/cactus/integration/maven/test/TestProperties.java
@@ -0,0 +1,52 @@
+/* 
+ * ========================================================================
+ * 
+ * Copyright 2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * ========================================================================
+ */
+
+package org.apache.cactus.integration.maven.test;
+
+import java.util.Map;
+
+import org.apache.cactus.ServletTestCase;
+import org.apache.cactus.WebRequest;
+
+/**
+ * Verify that properties defined by the 
+ * <code>cactus.sysproperties</code> are found in the resulting
+ * cactified WAR. Note that we're testing this by executing this code inside
+ * the container.
+ *
+ * @version $Id: TestProperties.java 239081 2004-11-08 22:02:37Z felipeal $
+ */
+public class TestProperties extends ServletTestCase
+{
+
+    /**
+     * Verify that the properties were set correctly.
+     */
+    public void testProperties()
+    {
+      String prop1 = System.getProperty("prop1");
+      String prop2 = System.getProperty("prop2");
+      assertNotNull( prop1 );
+      assertNotNull( prop2 );
+      assertEquals( "value1", prop1 );
+      assertEquals( "value2", prop2 );
+    }
+
+}
