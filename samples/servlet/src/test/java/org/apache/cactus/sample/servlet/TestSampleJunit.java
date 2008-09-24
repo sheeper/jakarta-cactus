5ac58286b2bb9e469b4954665d2515d3d2dae563
ptahchiev <ptahchiev@13f79535-47bb-0310-9956-ffa450edef68>
2008-09-24 13:20:11 +0000

    Moved the tests to the src/test/java directory, where they actually should be. Also changed the surefire plugin declaration to point to them.
    git-svn-id: http://svn.apache.org/repos/asf/jakarta/cactus/trunk@698559 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/servlet/src/test/java/org/apache/cactus/sample/servlet/TestSampleJunit.java
@@ -0,0 +1,14 @@
+package org.apache.cactus.sample.servlet;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.apache.cactus.ServletTestCase;
+
+public class TestSampleJunit
+{
+  @Test
+  public void blablaMetod() {
+    assertEquals(0,0);  
+  }
+
+}
